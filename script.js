d94e4a35b1e4cc967df9a36895f424b35706580e
jidnyasasathe8-max <jidnyasasathe8@gmail.com>
2026-06-22 18:39:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+# my second work on git hub
+# it is program about to implement matrix addition,matirx multiplication,transpose
+# in this program we use 2D arrays for better modulatory
+and this is how this program will be.
